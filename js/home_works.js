02e6e3a85a74eecc68e28c694012eea8c3a49c14
Kanatbekovak <kanatbekovakaniet@gmail.com>
2025-11-08 09:39:24 +0600

    dz2-dz3

--- js/home_works.js
+++ js/home_works.js
@@ -2,6 +2,11 @@ const gmailInput = document.getElementById("gmail_input");
 const gmailButton = document.getElementById("gmail_button");
 const gmailResult = document.getElementById("gmail_result");
 const childBlock = document.querySelector(".child_block");
+const buttonStart = document.getElementById("start");
+const buttonStop = document.getElementById("stop");
+const buttonReset = document.getElementById("reset");
+const intervalSecond = document.getElementById("seconds");
+
 
 const regExpGmail = /[a-z0-9.]+@(gmail|mail|icloud|yahoo)\.com/i;
 
@@ -15,67 +20,182 @@ gmailButton.onclick = () => {
   }
 };
 
-
-// let arr = 0
-
-// const changeBlock = () => {
-//   arr++;
-//   childBlock.style.left = `${arr}px`;
-//   if(arr<449 ){
-//     requestAnimationFrame(changeBlock)
-//   }
-// }
-// changeBlock()
+//dz1 
 
 
-const parentBlock = document.querySelector('.parent_block');
+let arr = 0;
 
-let x = 0;
-let y = 0;
-let side = 0;
-
-const moveBlock = () => {
-  const maxX = parentBlock.clientWidth - childBlock.offsetWidth;
-  const maxY = parentBlock.clientHeight - childBlock.offsetHeight;
-
-  if(side === 0) x++;
-  else if(side === 1) y++;
-  else if(side === 2) x--;
-  else if(side === 3) y--;
-
-  childBlock.style.left = x + 'px';
-  childBlock.style.top = y + 'px';
-
-  if(side === 0 && x >= maxX) side = 1;
-  else if(side === 1 && y >= maxY) side = 2;
-  else if(side === 2 && x <= 0) side = 3;
-  else if(side === 3 && y <= 0) side = 0;
-
-  requestAnimationFrame(moveBlock);
-};
-
-moveBlock();
+function changeBlockleft() {
+  const change = setInterval(() => {
+    arr++;
+    childBlock.style.left = `${arr}px`;
+    if (arr >= 449) {
+      clearInterval(change);
+      arr = 0;
+      changeBlockbottom(); 
+    }
+  }, 10);
+}
 
 
+function changeBlockbottom() {
+  const change = setInterval(() => {
+    arr++;
+    childBlock.style.top = `${arr}px`;
+    if (arr >= 449) {
+      clearInterval(change);
+      arr = 449;
+      changeBlockright();
+    }
+  }, 10);
+}
 
+
+function changeBlockright() {
+  const change = setInterval(() => {
+    arr--;
+    childBlock.style.left = `${arr}px`;
+    if (arr <= 0) {
+      clearInterval(change);
+      arr = 449;
+      changeBlocktop();
+    }
+  }, 10);
+}
+
+
+function changeBlocktop() {
+  const change = setInterval(() => {
+    arr--;
+    childBlock.style.top = `${arr}px`;
+    if (arr <= 0) {
+      clearInterval(change);
+      arr = 0;
+      changeBlockleft();
+    }
+  }, 10);
+}
+
+
+changeBlockleft();
+
+
+//dz2
+
+let intervalid = null;
+
+buttonStart.addEventListener("click", () => {
+  if(intervalid) return;
+  intervalid = setInterval(() => {
+    let value = Number (intervalSecond.innerText);
+    value++;
+    intervalSecond.innerText = value;
+  },5)
+})
+
+buttonReset.addEventListener("click", () => {
+  clearInterval(intervalid);
+  intervalid = null;
+  intervalSecond.innerText = "0"
+  },5)
+
+
+buttonStop.addEventListener("click", () => {
+  clearInterval(intervalid);
+  intervalid = null;
+})
+
+
+//////////////dz3
+
+
+const characters = [
+  {
+    name: 'Alina',
+    age: 16,
+    image: "https://cdn-icons-png.flaticon.com/256/11565/11565158.png",
+  },
+  {
+    name: 'Bruno',
+    age: 26,
+    image:"https://cdn-icons-png.flaticon.com/256/11565/11565162.png",
+  },
+  {
+    name: 'Kaniet',
+    age: 18,
+    // image:"https://cdn-icons-png.flaticon.com/256/11565/11565146.png",
+  },
+  {
+    name: 'Lina',
+    age: 50,
+    image:"https://cdn-icons-png.flaticon.com/256/11565/11565108.png",
+  },
+  {
+    name: 'Milana',
+    age: 6,
+    image:"https://cdn-icons-png.flaticon.com/256/11565/11565114.png",
+  },
+  {
+    name: 'Rustam',
+    age: 41,
+    image:"https://cdn-icons-png.flaticon.com/256/11565/11565136.png",
+  },
+  {
+    name: 'Nuraman',
+    age: 13,
+    image:"https://cdn-icons-png.flaticon.com/256/11565/11565113.png",
+  },
+  {
+    name: 'Tina',
+    age: 37,
+    image:"https://cdn-icons-png.flaticon.com/256/11565/11565138.png",
+  },
+  {
+    name: 'Jun',
+    age: 79,
+    image:"https://cdn-icons-png.flaticon.com/256/11565/11565143.png",
+  }
+]
+
+
+
+const defaultImg = "https://www.svgrepo.com/show/452030/avatar-default.svg"
+
+
+const charactersList = document.querySelector(".characters-list");
+
+characters.forEach((character) => {
+  const characterCard = document.createElement("div");
+  characterCard.setAttribute("class", "character-card");
+
+  characterCard.innerHTML = `
+    <div class="character-photo">
+      <img src="${character.image ?? defaultImg}" alt="${character.name}">
+    </div>
+    <p><strong>${character.name}</strong></p>
+    <p>Age: ${character.age}</p>
+  `;
+
+  charactersList.appendChild(characterCard);
+});
 
 
 const added = () => {
-  const photos = document.querySelectorAll(".character-card .character-photo");
+  const photos = document.querySelectorAll(".character-card");
   const picture = 3;
   let index = 0;
-
   const showPhotos = () => {
     photos.forEach(photo => photo.style.display = "none");
 
-    for (let i = 0; i < picture; i++) {
-      const photoIndex = (index + i) % photos.length;
-      photos[photoIndex].style.display = "block";
+    for (let i = index; i < index + picture && i < photos.length; i++) {
+      photos[i].style.display = "block";
     }
+    index += picture;
 
-    index = (index + picture) % photos.length;
-  };
-
+    if (index >= photos.length) {
+      index = 0;
+    }
+  }
   showPhotos();
 
   setInterval(showPhotos, 5000);
@@ -84,36 +204,103 @@ const added = () => {
 added();
 
 
-//////////
+// //////////
 
 const modal = document.querySelector('.modal');
 const closeButton = modal.querySelector('.modal_close');
+const btnOpen = document.querySelector("#btn-get");
 
-function openModal() {
-  modal.style.display = 'flex';
+
+openModal = () => {
+  modal.style.display= "block";
+  document.body.style.overflow = "hidden"
 }
 
-function closeModal() {
-  modal.style.display = 'none';
+
+closeModal = () => {
+  modal.style.display = "none"
+  document.body.style.overflow = ""
 }
 
-closeButton.addEventListener('click', closeModal);
+btnOpen.addEventListener('click',() => {
+  openModal();
+});
 
-const modalTimerId = setTimeout(openModal, 5000);
+closeButton.addEventListener('click',() => {
+  closeModal();
+});
 
-
-function scrollHandler() {
-  if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
-    openModal();
-    clearTimeout(modalTimerId);
-    window.removeEventListener('scroll', scrollHandler);
+modal.addEventListener("click", (event) => {
+  if(event.target === modal) {
+    closeModal();
   }
+})
+
+let open = false;
+let modalBtn = false;
+let scrollEnd = false;
+let counter;
+
+const scroll = () => {
+    if (!open && !modalBtn && !scrollEnd) {
+        if (window.pageYOffset > 1000) {
+            setTimeout(() => {
+                openModal()
+            }, 1000);
+        }    
+    }
 }
 
-window.addEventListener('scroll', scrollHandler);
+window.addEventListener('scroll', scroll);
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight && !scrollEnd) {
+        scrollEnd = true;
+        clearTimeout(counter);
+        if (!modalBtn && !scroll) {
+            counter = setTimeout(() => {
+                openModal();
+            }, 10000);
+        }
+    } else if (!scrollEnd) {
+        clearTimeout(counter); 
+    }
+});
+
+counter = setTimeout(() => {
+    if (!modalBtn && !scrollEnd) {
+        openModal();
+    }
+}, 10000);
 
 
-//////////
 
-setTimeout(openModal, 10000);
+
+// function openModal() {
+//   modal.style.display = 'flex';
+// }
+
+// function closeModal() {
+//   modal.style.display = 'none';
+// }
+
+// closeButton.addEventListener('click', closeModal);
+
+// const modalTimerId = setTimeout(openModal, 5000);
+
+
+// function scrollHandler() {
+//   if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+//     openModal();
+//     clearTimeout(modalTimerId);
+//     window.removeEventListener('scroll', scrollHandler);
+//   }
+// }
+
+// window.addEventListener('scroll', scrollHandler);
+
+
+// //////////
+
+// setTimeout(openModal, 10000);
 
---
+++ js/modal.js
@@ -0,0 +1,27 @@
+// const modal = document.querySelector('.modal')
+// const btnOpen = document.querySelector('#btn-get');
+// const btnClose = document.querySelector('.modal_close');
+
+// const openModal = () => {
+//   modal.style.display = 'block';
+//   document.body.style.overflow = 'hidden';
+// }
+
+// const closeModal = () => {
+//   modal.style.display = 'none';
+//   document.body.style.overflow = '';
+// }
+
+// btnOpen.addEventListener('click',() => {
+//   openModal();
+// }) 
+
+// btnClose.addEventListener('click', () => {
+//   closeModal();
+// })
+
+// modal.addEventListener('click', (event) => {
+//   if(event.target == modal){
+//     closeModal();
+//   }
+// })
